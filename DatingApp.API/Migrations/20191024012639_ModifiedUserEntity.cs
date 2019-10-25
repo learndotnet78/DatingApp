5968ece17953b4c95fbddfb16951f5aa0e957d0a
learndotnet78 <learndotnet78@hotmail.com>
2019-10-24 23:03:45 -0400

    New Commit

---
+++ DatingApp.API/Migrations/20191024012639_ModifiedUserEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class ModifiedUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PasswordSalt",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+        }
+    }
+}
